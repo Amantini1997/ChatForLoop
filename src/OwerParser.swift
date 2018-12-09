f8d33a424252de10bcb4c5abbdbe26d48996137c
k1763585 <alessandro.amantini@kcl.ac.uk>
2018-12-09 07:21:03 +0000

    Merge ious when equal

--- src/OwerParser.swift
+++ src/OwerParser.swift
@@ -11,8 +11,10 @@ class OwerParser{
     var receivable:String
     var owePos:Int
     var atPos:Int
+    var iou:IOU
 
     init(){
+        iou = IOU()
         quantity = 0
         dQuantity = -1.0
         object = ""
@@ -23,7 +25,7 @@ class OwerParser{
         atPos = 0;
     }
 
-    func parse(x: String) -> (String,String,String,String)?{
+    func parse(x: String){
         var arr = x.components(separatedBy:" ")
         if x.contains(" @"){
             if x.contains(" owe ") || x.contains(" ought "){
@@ -31,16 +33,16 @@ class OwerParser{
                 let pre = arr[owePos-1].lowercased()
                 let post = arr[owePos+1].lowercased()
                 if !["you","u","i"].contains(pre){
-                    return nil //print("NOT VALID")//replace whit return
+                    return//print("NOT VALID")//replace whit return
                 }else if !["you","u","me"].contains(post){
-                    return nil //print("NOT VALID")//replace with return
+                    return//print("NOT VALID")//replace with return
                 }
                 if pre == "you" || pre == "u"{
                     receiver = "me"
-                    ower = "You"
+                    ower = "you"
                 }else /*post == "you" || post == "u"*/{
                     receiver = "you"
-                    ower = "I"
+                    ower = "i"
                 }
             }
             else if x.contains(" o "){
@@ -50,17 +52,17 @@ class OwerParser{
                 let post = arr[owePos+1].lowercased()
                 if pre == "you" || pre == "u"{
                     receiver = "me"
-                    ower = "You"
+                    ower = "you"
                 }
                 else if post == "you" || post == "u"{
                     receiver = "you"
-                    ower = "I"
+                    ower = "i"
                 }
             }else{
-                return nil //print("NOT VALID")//replace
+                return//print("NOT VALID")//replace
             }
         }else{
-            return nil //print("NOT VALID")//replace
+            return//print("NOT VALID")//replace
         }
 
         if ower != "" {
@@ -106,10 +108,92 @@ class OwerParser{
         }
         let stringQuantity = (dQuantity < Double(quantity)) ? String(quantity) : String(dQuantity)
         //print("\(ower) owe \(receiver) \(stringQuantity) \(receivable)")
-        return(ower,receiver,receivable,stringQuantity)
+        iou.add(x:(ower,receiver,receivable,stringQuantity))
+    }
+
+    func getAllIous(){
+        for i in iou.getAll(){
+            print(i)
+        }
     }
 }
 
-/*
-var parser = OwerParser()
-parser.parse(x:"Hello mother fucker, do you remember that you owe me 2 fucking @pounds??")*/
+class IOU{
+    var ious:Array<(String,String,String,String)>
+    init(){
+        ious = []
+    }
+
+    func add(x:(String,String,String,String)){
+        let count = ious.count
+        for i in 0..<count{
+            if ious[i].2 == x.2{
+                merge(x:x,i:i)
+                return
+            }else if(ious[i].2 == x.2+"s"){
+                merge(x:x,i:i)
+                return
+            }else if(ious[i].2+"s"==x.2){
+                merge(x:x,i:i)
+                return
+            }
+        }
+        ious.append(x)
+    }
+
+    func merge(x:(String,String,String,String),i:Int){
+        var mine = 0.0
+        var yours = 0.0
+        if(ious[i].0 == "you"){
+            mine += Double(ious[i].3)!
+        }else{
+            yours += Double(ious[i].3)!
+        }
+        if(x.0 == "you"){
+            mine += Double(x.3)!
+        }else{
+            yours += Double(x.3)!
+        }
+        let delta = mine - yours
+        let receivables = ious[i].2
+        ious.remove(at:i)
+        print(delta)
+        if delta>0 {
+            ious.append(("you","me",receivables,String(delta)))
+        }else if delta<0{
+            ious.append(("i","you",receivables,String(delta)))
+        }
+    }
+
+    func getter(ower:String) -> Array<(String,String,String,String)>{
+        var temp = Array<(String,String,String,String)>()
+        for iou in ious{
+            if ious[0].0 == ower{
+                temp.append(iou)
+            }
+        }
+        return temp
+    }
+
+    func getMyOwes()-> Array<(String,String,String,String)>{
+        return getter(ower:"I")
+    }
+
+    func getYourOwes()-> Array<(String,String,String,String)>{
+        return getter(ower:"you")
+    }
+
+    func getAll() ->Array<(String,String,String,String)>{
+        return ious
+    }
+}
+
+
+/*var parser = OwerParser()
+parser.parse(x:"Hello mother fucker, do you remember that you owe me 1 fucking @pounds??")
+parser.parse(x:"Hello mother fucker, do you remember that you owe me 2 fucking @pounds??")
+parser.parse(x:"Hello mother fucker, do you remember that I owe I 2 fucking @pounds??")
+parser.parse(x:"Hello mother fucker, do you remember that you owe me 2 fucking @pounds??")
+parser.parse(x:"Hello mother fucker, do you remember that you owe me 2 fucking @pounds??")
+parser.parse(x:"Hello mother fucker, do you remember that you owe me 2 fucking @pounds??")
+parser.getAllIous()*/
