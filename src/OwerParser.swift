8869c8fa3e5230f30a0bf881dff9d8ed7c6da829
k1763585 <alessandro.amantini@kcl.ac.uk>
2018-12-09 06:26:27 +0000

    Transform the parser into a class

--- src/OwerParser.swift
+++ src/OwerParser.swift
@@ -1,95 +1,114 @@
 import Foundation
 import Glibc
 
+class OwerParser{
 
-var x:String = "hey, you owe Josh 2 @pints and 3 , you cheap fuckhead"
-var quantity = 0
-var dQuantity = -1.0
-var object:String = ""
-var ower = ""
-var receiver = ""
-var receivable = ""
-var arr = x.components(separatedBy:" ")
-var owePos = 0;
-var atPos = 0;
-if x.contains(" @"){
-    if x.contains(" owe ") || x.contains(" ought "){
-        owePos = (x.contains(" owe ")) ? arr.index(of:"owe")! : arr.index(of:"ought")!
-        let pre = arr[owePos-1].lowercased()
-        let post = arr[owePos+1].lowercased()
-        if !["you","u","i"].contains(pre){
-            print("NOT VALID")//replace whit return
-        }else if !["you","u","me"].contains(post){
-            print("NOT VALID")//replace with return
-        }
-        if pre == "you" || pre == "u"{
-            receiver = "me"
-            ower = "You"
-        }else /*post == "you" || post == "u"*/{
-            receiver = "you"
-            ower = "I"
-        }
+    var quantity:Int
+    var dQuantity:Double
+    var object:String
+    var ower:String
+    var receiver:String
+    var receivable:String
+    var owePos:Int
+    var atPos:Int
+
+    init(){
+        quantity = 0
+        dQuantity = -1.0
+        object = ""
+        ower = ""
+        receiver = ""
+        receivable = ""
+        owePos = 0;
+        atPos = 0;
     }
-    else if x.contains(" o "){
+
+    func parse(x: String) -> (String,String,String,String)?{
         var arr = x.components(separatedBy:" ")
-        owePos = arr.index(of:"o")!
-        let pre = arr[owePos-1].lowercased()
-        let post = arr[owePos+1].lowercased()
-        if pre == "you" || pre == "u"{
-            receiver = "me"
-            ower = "You"
+        if x.contains(" @"){
+            if x.contains(" owe ") || x.contains(" ought "){
+                owePos = (x.contains(" owe ")) ? arr.index(of:"owe")! : arr.index(of:"ought")!
+                let pre = arr[owePos-1].lowercased()
+                let post = arr[owePos+1].lowercased()
+                if !["you","u","i"].contains(pre){
+                    return nil //print("NOT VALID")//replace whit return
+                }else if !["you","u","me"].contains(post){
+                    return nil //print("NOT VALID")//replace with return
+                }
+                if pre == "you" || pre == "u"{
+                    receiver = "me"
+                    ower = "You"
+                }else /*post == "you" || post == "u"*/{
+                    receiver = "you"
+                    ower = "I"
+                }
+            }
+            else if x.contains(" o "){
+                var arr = x.components(separatedBy:" ")
+                owePos = arr.index(of:"o")!
+                let pre = arr[owePos-1].lowercased()
+                let post = arr[owePos+1].lowercased()
+                if pre == "you" || pre == "u"{
+                    receiver = "me"
+                    ower = "You"
+                }
+                else if post == "you" || post == "u"{
+                    receiver = "you"
+                    ower = "I"
+                }
+            }else{
+                return nil //print("NOT VALID")//replace
+            }
+        }else{
+            return nil //print("NOT VALID")//replace
         }
-        else if post == "you" || post == "u"{
-            receiver = "you"
-            ower = "I"
+
+        if ower != "" {
+            for i in 0..<arr.count{
+                var temp = arr[i]
+                if temp.starts(with:"@"){
+                    atPos = i
+                    while [",",";",":",".","?","!"].contains(temp.last!){
+                        temp = String(temp.dropLast())
+                    }
+                    receivable = String(temp.characters.suffix(temp.count-1))
+                    if temp.last != "s"{
+                        quantity = 1
+                    }
+                    continue
+                }
+            }
         }
-    }else{
-        print("NOT VALID")//replace
+        if dQuantity == -1.0{
+            for i in owePos...atPos{
+                let word = arr[i].lowercased()
+                if word.contains("couple") || word.contains("pair"){
+                    quantity = 2
+                    continue
+                }else if word == "an"{
+                    quantity = 1
+                    continue
+                }else if word.contains("."){
+                    dQuantity = Double(arr[atPos-1] as String) ?? -1.0
+                    continue
+                }/*else if(true){
+                    let number = NumberFormatter.number(from: word)
+                    quantity = number.integerValue ?? 0
+                    continue
+                }*/else{
+                    let num = Int(word)
+                    if num != nil {
+                        quantity = num!
+                    }
+                    continue
+                }
+            }
+        }
+        let stringQuantity = (dQuantity < Double(quantity)) ? String(quantity) : String(dQuantity)
+        print("\(ower) owe \(receiver) \(stringQuantity) \(receivable)")
+        return(ower,receiver,receivable,stringQuantity)
     }
-}else{
-    print("NOT VALID")//replace
 }
 
-if ower != "" {
-    for i in 0..<arr.count{
-        var temp = arr[i]
-        if temp.starts(with:"@"){
-            atPos = i
-            while [",",";",":",".","?","!"].contains(temp.last!){
-                temp = String(temp.dropLast())
-            }
-            receivable = String(temp.characters.suffix(temp.count-1))
-            if temp.last != "s"{
-                quantity = 1
-            }
-            continue
-        }
-    }
-}
-if dQuantity == -1.0{
-    for i in owePos...atPos{
-        let word = arr[i].lowercased()
-        if word.contains("couple") || word.contains("pair"){
-            quantity = 2
-            continue
-        }else if word == "an"{
-            quantity = 1
-            continue
-        }else if word.contains("."){
-            dQuantity = Double(arr[atPos-1] as String) ?? -1.0
-            continue
-        }/*else if(true){
-            let number = NumberFormatter.number(from: word)
-            quantity = number.integerValue ?? 0
-            continue
-        }*/else{
-            let num = Int(word)
-            if num != nil {
-                quantity = num!
-            }
-            continue
-        }
-    }
-}
-let stringQuantity = (dQuantity < Double(quantity)) ? String(quantity) : String(dQuantity)
-print("\(ower) owe \(receiver) \(stringQuantity) \(receivable)")
+var parser = OwerParser()
+parser.parse(x:"Hello mother fucker, do you remember that you owe me 2 fucking @pounds??")
